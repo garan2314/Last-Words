79aaba3b20242e293e6376d7e9f905e4288d5f03
lewistham <garan2314@gmail.com>
2018-02-25 14:59:12 +0800

    Initial Commit

---
+++ ThreeLetterKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThreeLetterKeyboard
+//
+//  Created by Lewis Tham on 25/2/18.
+//  Copyright © 2018 miim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
