e49801cde17c250ddda4a73f06f50c8280ad994d
lewistham <garan2314@gmail.com>
2018-03-05 02:53:59 +0800

    UI horizontal and layout change, 4 letters

--- ThreeLetterKeyboard/ViewController.swift
+++ ThreeLetterKeyboard/ViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     var newWord = Word()
-
+    var pageNo = 1
+    
     @IBOutlet weak var lbCurrWord: UILabel!
     @IBOutlet weak var tbCurrWord: UITextField!
     
     @IBOutlet weak var btLetter1: UIButton!
     @IBOutlet weak var btLetter2: UIButton!
     @IBOutlet weak var btLetter3: UIButton!
+    @IBOutlet weak var btLetter4: UIButton!
     @IBOutlet weak var btBestWord: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         update()
     }
     
@@ -34,6 +35,9 @@ class ViewController: UIViewController {
     }
     @IBAction func btLetter3(_ sender: Any) {
         type(letter: btLetter3.title(for: .normal)!)
+    }
+    @IBAction func btLetter4(_ sender: Any) {
+        type(letter: btLetter4.title(for: .normal)!)
     }
     @IBAction func btBestWord(_ sender: Any) {
         best()
@@ -86,33 +90,50 @@ class ViewController: UIViewController {
         //let letters = newWord.getNextFew()
         let letters = newWord.getLetters()
     
-        if letters.count>=1
+        let option1 = (pageNo-1)*4
+        let option2 = (pageNo-1)*4+1
+        let option3 = (pageNo-1)*4+2
+        let option4 = (pageNo-1)*4+3
+
+        
+        if letters.count>=option1+1
         {
-            btLetter1.setTitle(letters[0], for: .normal)
+            btLetter1.setTitle(letters[option1], for: .normal)
             btLetter1.isEnabled = true
         }
         else
         {
             clearOption(option: 1)
         }
-        if letters.count>=2
+        if letters.count>=option2+1
         {
-            btLetter2.setTitle(letters[1], for: .normal)
+            btLetter2.setTitle(letters[option2], for: .normal)
             btLetter2.isEnabled = true
         }
         else
         {
             clearOption(option: 2)
         }
-        if letters.count>=3
+        if letters.count>=option3+1
         {
-            btLetter3.setTitle(letters[2], for: .normal)
+            btLetter3.setTitle(letters[option3], for: .normal)
             btLetter3.isEnabled = true
         }
         else
         {
             clearOption(option: 3)
         }
+        
+        if letters.count>=option4+1
+        {
+            btLetter4.setTitle(letters[option4], for: .normal)
+            btLetter4.isEnabled = true
+        }
+        else
+        {
+            clearOption(option: 4)
+        }
+        
         
         
         let bestWord = newWord.getBestWord()
@@ -148,7 +169,7 @@ class ViewController: UIViewController {
         
     }
     
-    func clearOption(option : Int = 4)
+    func clearOption(option : Int = 0)
     {
         if option == 1
         {
@@ -166,6 +187,11 @@ class ViewController: UIViewController {
             btLetter3.isEnabled = false
         }
         else if option == 4
+        {
+            btLetter4.setTitle("", for: .normal)
+            btLetter4.isEnabled = false
+        }
+        else if option == 0
         {
             clearOption(option : 1)
             clearOption(option : 2)
